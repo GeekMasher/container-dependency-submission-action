bb4626bef48a657de62dc88a43c114b7b97e0e8b
GeekMasher <geekmasher@gmail.com>
2023-05-05 18:20:38 +0100

    feat: Initial Container Submission tool

---
+++ cdsa/__main__.py
@@ -0,0 +1,99 @@
+import os
+import logging
+import argparse
+import sys
+
+from ghastoolkit.octokit.dependencygraph import DependencyGraph
+from ghastoolkit.octokit.github import GitHub
+
+from cdsa import __name__ as name
+from cdsa.syft import Syft
+
+logger = logging.getLogger(name)
+parser = argparse.ArgumentParser(name)
+
+parser.add_argument("-c", "--container", type=str)
+parser.add_argument("-d", "--dockerfile", type=str)
+
+parser.add_argument("--debug", action="store_true", help="Debug mode")
+parser.add_argument("--sha", default=os.environ.get("GITHUB_SHA"), help="Commit SHA")
+parser.add_argument("--ref", default=os.environ.get("GITHUB_REF"), help="Commit ref")
+
+parser_github = parser.add_argument_group("GitHub")
+parser_github.add_argument(
+    "-gr",
+    "--github-repository",
+    default=os.environ.get("GITHUB_REPOSITORY"),
+    help="GitHub Repository",
+)
+parser_github.add_argument(
+    "-gi",
+    "--github-instance",
+    default=os.environ.get("GITHUB_API_URL", "https://api.github.com"),
+    help="GitHub Instance",
+)
+parser_github.add_argument(
+    "-t",
+    "-gt",
+    "--github-token",
+    default=os.environ.get("GITHUB_TOKEN"),
+    help="GitHub API Token",
+)
+
+
+if __name__ == "__main__":
+    arguments = parser.parse_args()
+    logging.basicConfig(
+        level=logging.DEBUG if arguments.debug or os.environ.get("DEBUG") else logging.INFO,
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    )
+
+    GitHub.init(
+        arguments.github_repository,
+        reference=arguments.ref
+    )
+    logging.info(f"Repository :: {GitHub.repository}")
+
+    depgraph = DependencyGraph()
+
+    syft = Syft()
+
+    if not syft.check():
+        syft.install()
+
+    logging.debug(f"Syft Path :: {syft.binary}")
+
+    sbom = None
+
+    if arguments.container:
+        logging.info("In container mode...")
+
+        logging.info(f"Starting SBOM generation")
+        sbom = syft.generateSBOM(arguments.container)
+
+    elif arguments.dockerfile:
+        logging.info("In Dockerfile mode")
+        logging.warning("This mode automatically builds the docker image which can have issues")
+
+        name = syft.buildContainer(arguments.dockerfile) 
+
+        sbom = syft.generateSBOM(name)
+         
+        for manifest, data in sbom.get("manifests", {}).items():
+            data["file"] = {
+                "source_location": arguments.dockerfile
+            }
+            sbom["manifests"][manifest] = data
+
+
+    if sbom:
+        logging.info("Uploading SBOM...")
+
+        depgraph.submitSbom(sbom)
+
+    else:
+        logging.error("Failed to generate SBOM!")
+        sys.exit(1)
+
+    logging.info("Completed")
+
---
+++ cdsa/syft.py
@@ -0,0 +1,74 @@
+import os
+import json
+import tempfile
+import logging
+import subprocess
+
+logger = logging.getLogger("cdsa.syft")
+
+LOCATIONS = [
+    "/usr/local/bin",
+    os.path.expanduser("~/.local/bin")
+]
+
+class Syft():
+    def __init__(self) -> None:
+        self.name = "syft"
+        self.path = None
+
+    @property
+    def binary(self) -> str:
+        if self.path:
+            return self.path
+        return self.name
+
+    def check(self):
+        for loc in LOCATIONS:
+            fullpath = os.path.join(loc, "syft")
+            if os.path.exists(fullpath):
+                self.path = fullpath
+                return True
+        try:
+            subprocess.check_call(self.binary)
+            return True
+        except:
+            logger.debug("Syft not found on the system")
+        return False
+
+    def install(self):
+        """Install syft via Brew"""
+        # https://github.com/actions/runner-images/blob/main/images/linux/Ubuntu2204-Readme.md#package-management
+        # https://github.com/anchore/syft#homebrew
+        cmd = ["brew", "install", "syft"]
+        subprocess.run(cmd, check=True)
+
+
+    def generateSBOM(self, image: str) -> dict:
+        output = os.path.join(tempfile.gettempdir(), "syft-sbom.json")
+        cmd = [self.binary, image, "-o", f"github-json={output}"]
+
+        logger.debug(f"SBOM Command :: {cmd}")
+        
+        with open(os.devnull, 'w') as null:
+            subprocess.run(cmd, stdout=null, stderr=null)
+
+        if os.path.exists(output):
+            with open(output, "r") as handle:
+                return json.load(handle)
+
+        return {}
+
+    def buildContainer(self, path: str) -> str:
+        logger.info(f"Building container from path :: {path}")
+
+        name = "cdsa-image:latest"
+        cmd = ["docker", "build", "-t", name, "-f", path, os.getcwd()]
+
+        logger.debug(f"Container Build Command :: `{cmd}`")
+        
+        subprocess.run(cmd, check=True)
+
+        logger.info("Build command finished")
+        return name
+
+
